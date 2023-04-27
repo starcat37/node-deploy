49d79291c91cb044be0348d8b52b3d5e3c246b03
starcat37 <starcat37@korea.ac.kr>
2023-04-28 01:01:32 +0900

    [Typo] app.js 수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
-const nunjunks = require('nunjucks');
+const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
 const helmet = require('helmet');
@@ -13,46 +13,46 @@ const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
 const redisClient = redis.createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    password: process.env.REDIS_PASSWORD,
-    legacyMode: true,
-})
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
-const { sequelize } = require("./models");
+const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 const logger = require('./logger');
 
 const app = express();
-passportConfig(); //패스포트 설정
+passportConfig(); // 패스포트 설정
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
-nunjunks.configure('views', {
-    express: app,
-    watch: true,
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
 });
+
 sequelize.sync({ force: false })
-    .then(() => {
-        console.log('데이터베이스 연결 성공');
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(morgan('combined'));
-    app.use(
-        helmet({
-            contentSecurityPolicy: false,
-            crossOriginEmbedderPolicy: false,
-            crossOriginResourcePolicy: false,
-        }),
-    );
-    app.use(hpp());
+  app.use(morgan('combined'));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+    }),
+  );
+  app.use(hpp());
 } else {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
@@ -60,18 +60,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 const sessionOption = {
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-    store: new RedisStore({ client: redisClient }),
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  store: new RedisStore({ client: redisClient }),
 };
 if (process.env.NODE_ENV === 'production') {
-    sessionOption.proxy = true;
-    // sessionOption.cookie.secure = true;
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
 }
 app.use(session(sessionOption));
 app.use(passport.initialize());
@@ -83,18 +83,19 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    logger.info('hello');
-    logger.error(error.message);
-    next(error);
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  logger.info('hello');
+  logger.error(error.message);
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+  console.error(err);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
