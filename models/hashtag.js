33b7acdd6c2cfa4df8f2fc88bb522ea3c9d12279
starcat37 <starcat37@korea.ac.kr>
2023-05-02 14:54:23 +0900

    뒤엎기...

--- models/hashtag.js
+++ models/hashtag.js
@@ -1,28 +1,26 @@
 const Sequelize = require('sequelize');
 
-class Hashtag extends Sequelize.Model {
-    static initiate(sequelize) {
-        Hashtag.init({
-            title: {
-                type: Sequelize.STRING(15),
-                allowNull: false,
-                unique: true,
-            },
-        }, {
-            sequelize,
-            timestamps: true,
-            underscored: false,
-            modelName: 'Hashtag',
-            tableName: 'hashtags',
-            paranoid: false,
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci',
-        });
-    }
+module.exports = class Hashtag extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      title: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        unique: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Hashtag',
+      tableName: 'hashtags',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
 
-    static associate(db) {
-        db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
-    }
+  static associate(db) {
+    db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
+  }
 };
-
-module.exports = Hashtag;
